953301b3a4bd255fc7433b8112ecd299f10ca1a7
Alexander Tsiklidis <alex@Alexanders-Air.lan>
2023-09-24 02:51:43 -0400

    finishes this lab partially a bit from done

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -1,12 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 const Post = ({ title, location, content, image }) => {
   const [showContent, setShowContent] = useState(false);
-
-  useEffect(() => {
-    // Effect to hide content when component mounts
-    setShowContent(false);
-  }, []);
 
   const toggleContent = () => {
     setShowContent(!showContent);
@@ -16,7 +11,7 @@ const Post = ({ title, location, content, image }) => {
     <div className="post">
       <h2>{title}</h2>
       <p className="location">{location}</p>
-      {showContent ? <p>{content}</p> : null}
+      <p className={showContent ? 'visible' : 'hidden'}>{content}</p>
       <img src={image} alt={title} />
       <button onClick={toggleContent}>
         {showContent ? 'Hide Content' : 'Go to Post'}
