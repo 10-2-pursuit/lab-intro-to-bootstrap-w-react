8f2b835a9a73fe34adf48da5ae9b58e0405de4ed
Addis Jackson <addisjackson@pursuit.org>
2023-10-02 12:52:15 -0400

    finishes lab

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,6 @@
-
 import React, { useState } from 'react';
 
-const SearchBar = ({ handleSearch }) => {
+const SearchBar = ({ handleSearch, filteredPosts }) => {
   const [query, setQuery] = useState('');
 
   const cancelSearch = () => {
@@ -15,14 +14,21 @@ const SearchBar = ({ handleSearch }) => {
     handleSearch({ target: { value: newQuery } });
   };
 
-
-return (
+  return (
     <div className="container-fluid">
       <div className="row">
         <div className="col-lg-12">
-        <div className="input-group" style={{ width: '1250px', alignContent: 'center', padding: '10px', margin: '10 5 10 5'  }}>
-
-            <label htmlFor="search" className="form-label" style={{ margin: '5px', fontStyle: 'italic', verticalAlign: 'center' }}>Search posts for location...</label>
+          <div
+            className="input-group"
+            style={{ width: '1250px', alignContent: 'center', padding: '10px', margin: '10px 5px' }}
+          >
+            <label
+              htmlFor="search"
+              className="form-label"
+              style={{ margin: '5px', fontStyle: 'italic', verticalAlign: 'center' }}
+            >
+              Search posts for location...
+            </label>
             <input
               id="search"
               className="form-control flex-grow-1"
@@ -35,6 +41,11 @@ return (
               Cancel
             </button>
           </div>
+          {filteredPosts.length === 0 && query.length > 0 && (
+            <div className="alert alert-warning mt-3">
+              No posts found with the search term "{query}".
+            </div>
+          )}
         </div>
       </div>
     </div>
